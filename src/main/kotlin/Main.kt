f48a5f490e8a6fc58335d06ce53e99e3abad3a71
TheShinySnivy <magicwand452@gmail.com>
2022-06-09 14:09:45 -0700

    Parse and Interp complete. Added Test cases and tidied documentation.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
 package main.kotlin
 
-import java.util.DoubleSummaryStatistics
-
-
+/**
+ * The base type for an expression.
+ * An expression constitutes a number, a boolean,
+ * an if statement, a variable or function name,
+ * a lambda, or a function call.
+ */
 interface ExprC {
+
+    /**
+     * Interprets the given expression
+     * using the given environment, in order
+     * to evaluate what Value the expression
+     * represents.
+     */
     fun interp(env : Map<String, Value>) : Value
 }
 class NumC(private val n : Double) : ExprC {
@@ -11,20 +21,28 @@ class NumC(private val n : Double) : ExprC {
     override fun interp(env: Map<String, Value>): Value {
         return NumV(n)
     }
-}
 
-class StrC(private val s : String) : ExprC {
-    override fun interp(env: Map<String, Value>): Value {
-        return StrV(s)
+    override fun toString(): String {
+        return "NumC($n)"
     }
 }
 
+/**
+ * A boolean expression.
+ */
 class BoolC(private val b : Boolean) : ExprC {
     override fun interp(env: Map<String, Value>): Value {
         return BoolV(b)
     }
+
+    override fun toString(): String {
+        return "BoolC($b)"
+    }
 }
 
+/**
+ * An if expression.
+ */
 class IfC(private val ifCond : ExprC, private val ifT: ExprC, private val ifF: ExprC) : ExprC {
     override fun interp(env: Map<String, Value>): Value {
         val res : Value = ifCond.interp(env)
@@ -39,7 +57,15 @@ class IfC(private val ifCond : ExprC, private val ifT: ExprC, private val ifF: E
             throw error("condition is not boolean");
         }
     }
+
+    override fun toString(): String {
+        return "IfC($ifCond, $ifT, $ifF)"
+    }
 }
+
+/**
+ * A function or variable name expression.
+ */
 class IdC(private val id : String) : ExprC {
     override fun interp(env: Map<String, Value>): Value {
         val result = env[id]
@@ -49,14 +75,28 @@ class IdC(private val id : String) : ExprC {
             return result;
         }
     }
+
+    override fun toString(): String {
+        return "IdC($id)"
+    }
 }
 
+/**
+ * A lambda expression.
+ */
 class LamC(private val args : List<String>, private val body : ExprC) : ExprC {
     override fun interp(env: Map<String, Value>): Value {
         return ClosV(args, body, env)
     }
+
+    override fun toString(): String {
+        return "LamC($args, $body)"
+    }
 }
 
+/**
+ * A function call expression.
+ */
 class AppC(val func : ExprC, val args : List<ExprC>) : ExprC {
     override fun interp(env: Map<String, Value>): Value {
         val application : Value = func.interp(env)
@@ -71,12 +111,25 @@ class AppC(val func : ExprC, val args : List<ExprC>) : ExprC {
             error("incorrect AppC format")
         }
     }
+
+    override fun toString(): String {
+        return "AppC($func, $args)"
+    }
+
+
 }
 
-
+/**
+ * The base type for a value. Expressions are evaluated into values
+ * until eventually only one value remains: the result of the program.
+ */
 interface Value {
     fun serialize() : String
 }
+
+/**
+ * The number value, effectively a Double.
+ */
 class NumV(val n: Double) : Value {
     constructor(m: Int) : this(m.toDouble())
     override fun serialize(): String {
@@ -86,6 +139,10 @@ class NumV(val n: Double) : Value {
         return this.serialize()
     }
 }
+
+/**
+ * The boolean value.
+ */
 class BoolV(val b : Boolean) : Value {
     override fun serialize(): String {
         return b.toString()
@@ -94,14 +151,10 @@ class BoolV(val b : Boolean) : Value {
         return this.serialize()
     }
 }
-class StrV(private val s : String) : Value {
-    override fun serialize(): String {
-        return "\"" + s + "\""
-    }
-    override fun toString(): String {
-        return this.serialize()
-    }
-}
+
+/**
+ * The value for a primitive operation.
+ */
 class PrimV(val s : String) : Value {
     override fun serialize(): String {
         return "#<primop>"
@@ -110,6 +163,10 @@ class PrimV(val s : String) : Value {
         return this.serialize()
     }
 }
+
+/**
+ * The value for a lambda expression
+ */
 class ClosV(val args : List<String>, val body : ExprC, val env : Map<String, Value>) : Value {
     override fun serialize(): String {
         return "#<procedure>"
@@ -119,6 +176,12 @@ class ClosV(val args : List<String>, val body : ExprC, val env : Map<String, Val
     }
 }
 
+/**
+ * The top-level environment.
+ * The environment indicates what
+ * symbols translate into what functions
+ * or other values.
+ */
 val topEnv: Map<String, Value> = mapOf(
     "+" to PrimV("+"),
     "-" to PrimV("-"),
@@ -131,6 +194,10 @@ val topEnv: Map<String, Value> = mapOf(
     "error" to PrimV("error")
 )
 
+/**
+ * Adds a list of symbols and their corresponding values to the environment.
+ * This allows the user to define names of parameters.
+ */
 fun addToEnv(env : Map<String, Value>, symbols : List<String>, values : List<Value>) : Map<String, Value> {
     if (symbols.size > values.size) error("no values to add")
     if (symbols.size < values.size) error("no symbols to add")
@@ -141,6 +208,11 @@ fun addToEnv(env : Map<String, Value>, symbols : List<String>, values : List<Val
     }
     return newEnv
 }
+
+/**
+ * Given a primitive operation and its arguments,
+ * evaluates the result.
+ */
 fun evalPrimV(s : String, args : List<Value>) : Value {
     if ((args[0] is NumV) and (args[1] is NumV)) {
         when (s) {
@@ -174,13 +246,73 @@ fun evalPrimV(s : String, args : List<Value>) : Value {
     error("should not get here")
 }
 
+/**
+ * Checks if a list can be parsed into an if expression
+ */
+fun isIf(sexp : List<*>) : Boolean{
+    return (sexp.size == 4) && sexp[0] == "if"
+}
+
+/**
+ * Checks if a list can be parsed into a lambda expression
+ */
+fun isLambda(sexp : List<*>) : Boolean{
+    return  sexp[(sexp.size - 2)] == "=>"
+}
+
+/**
+ * Takes a list and parses it into a series of expressions,
+ * which can then be interpreted into a result value.
+ */
+fun parse (sexp : Any) : ExprC {
+
+    when (sexp){
+        is Int -> return NumC(sexp)
+        is Double -> return NumC(sexp)
+        is String -> {
+            return IdC(sexp)
+        }
+        is Boolean -> return BoolC(sexp)
+        is List<*> -> {
+            //if(isBinaryOp(sexp)){
+            //    return AppC(parse((sexp[0] as Any)), listOf(parse(sexp[1] as Any), parse(sexp[2] as Any)) )
+            //}
+            //else
+            if (isIf(sexp)){
+                return IfC(parse((sexp[1] as Any)), parse((sexp[2] as Any)), parse((sexp[3] as Any)))
+            }
+            else if (isLambda(sexp)){
+                return LamC((sexp.subList(0, (sexp.size - 2)) as List<String>) , parse((sexp[(sexp.size - 1)]) as Any))
+            }
+            else{
+                val parsedFun = parse((sexp[0] as Any))
+
+                val args = sexp.subList(1, sexp.size)
+
+                var parsedArgs : List<ExprC> = listOf()
+
+                for (s: Any in (args as List<Any>))
+                {
+                    parsedArgs = parsedArgs + parse (s as Any)
+                }
+                return AppC(parsedFun, parsedArgs)
+            }
+
+        }
+        else -> {
+            return NumC(1)
+        }
+
+    }
+}
+
+
+
 fun main(args: Array<String>) {
 
     val testEnv : Map<String, Value> = mapOf(
         "x" to NumV(8)
     ) + topEnv
-
-    println("Hello World!")
 
     val v = NumV(5.0)
     println(v)
@@ -189,11 +321,6 @@ fun main(args: Array<String>) {
     val x = IfC(BoolC(false), NumC(1), NumC(2)).interp(testEnv)
     println(w)
     println(x)
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-
 
 }
 
--- src/main/kotlin/MainKtTest.kt
+++ src/main/kotlin/MainKtTest.kt
@@ -10,13 +10,71 @@ val testEnv : Map<String, Value> = mapOf(
 class Tests {
 
 
+    @Test
+    fun basicTests() {
+        assertEquals("4.0", NumC(4)
+            .interp(testEnv).serialize())
+
+        val x = AppC(LamC(listOf("x"),
+            AppC(IdC("+"), listOf(IdC("x"), NumC(1)))),
+            listOf(NumC(8)))
+        assertEquals("9.0", x.interp(topEnv).serialize())
+    }
 
     @Test
-    fun x() {
-        assertEquals("4.0", NumC(4).interp(testEnv).serialize())
+    fun binaryOperations() {
+        assertEquals(parse(listOf("+", 1, 2))
+            .interp(topEnv).serialize(), "3.0")
+        assertEquals(parse(listOf("-", 3, 0.5))
+            .interp(topEnv).serialize(), "2.5")
+        assertEquals(parse(listOf("*", 2, 3))
+            .interp(topEnv).serialize(), "6.0")
+        assertEquals(parse(listOf("/", 8, 2))
+            .interp(topEnv).serialize(), "4.0")
 
-        val x = AppC(LamC(listOf("x"), AppC(IdC("+"), listOf(IdC("x"), NumC(1)))), listOf(NumC(8)))
-        assertEquals("9.0", x.interp(topEnv).serialize())
+        assertEquals(parse(listOf("<=", 2, 8))
+            .interp(topEnv).serialize(), "true")
+        assertEquals(parse(listOf("<=", 8, 2))
+            .interp(topEnv).serialize(), "false")
+        assertEquals(parse(listOf("equal?", 2, 2))
+            .interp(topEnv).serialize(), "true")
+        assertEquals(parse(listOf("equal?", 1, 2))
+            .interp(topEnv).serialize(), "false")
+
+    }
+
+    @Test
+    fun parseTest() {
+        assertTrue(isLambda(listOf("=>", 1)))
+        assertEquals(parse(listOf("if", true, 2.0, 1.0)).toString(),
+            "IfC(BoolC(true), NumC(2.0), NumC(1.0))")
+        assertEquals(parse(listOf("+", 2.0, 1.0)).toString(),
+            "AppC(IdC(+), [NumC(2.0), NumC(1.0)])")
+    }
+
+    @Test
+    fun complexProgramTests() {
+        assertEquals(parse(listOf("+", 2.0, 1.0))
+            .interp(testEnv).serialize(), "3.0")
+        //(top-interp '(+ 2 1))
+
+        assertEquals(parse(listOf(listOf("+", "-", "=>",
+            listOf("+", 3, listOf("-", 6, 4))), "-", "+"))
+            .interp(topEnv).toString(), "-7.0")
+        //(top-interp '(var ((+ = -) (- = +)) in (+ 3 (- 6 4))))
+
+        assertEquals(parse(
+            listOf(listOf( "a", "b", "c", "=>",
+                listOf("+", "a", listOf(listOf("x", "y", "=>",
+                    listOf("*", "x", "y")), 3, 3 ))), 2, 4, 5))
+            .interp(topEnv).serialize(), "11.0")
+        //(top-interp '((a b c => (+ a ((x y => (* x y)) 3 3))) 2 4 5))
+
+        assertEquals(parse(
+            listOf(listOf(listOf(listOf("x", "=>", listOf("y", "=>",
+                listOf("z", "=>", listOf("+", "x", listOf("+", "y", "z"))))), 2), 3), 4)
+        ).interp(topEnv).serialize(), "9.0")
+        //(top-interp '((((x => (y => (z => (+ x (+ y z))))) 2) 3) 4))
     }
 
 }
